1562e78149d2c4e7cb0aeb6470e69fe2b9be8161
haimy02 <164890342+haimy02@users.noreply.github.com>
2024-04-27 09:49:21 +0700

    Add files via upload

---
+++ TTCN/Forms/Congviec.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace TTCN.Forms
+{
+    partial class Congviec
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtMaCV = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtTenCV = new System.Windows.Forms.TextBox();
+            this.txtHeSoLuong = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dgridCongViec = new System.Windows.Forms.DataGridView();
+            this.btnThem = new System.Windows.Forms.Button();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.btnSua = new System.Windows.Forms.Button();
+            this.btnLuu = new System.Windows.Forms.Button();
+            this.btnBoqua = new System.Windows.Forms.Button();
+            this.btnDong = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgridCongViec)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(236, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(335, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "DANH MỤC CÔNG VIỆC";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(25, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(109, 19);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Mã công viêc: ";
+            // 
+            // txtMaCV
+            // 
+            this.txtMaCV.Location = new System.Drawing.Point(140, 77);
+            this.txtMaCV.Name = "txtMaCV";
+            this.txtMaCV.Size = new System.Drawing.Size(227, 22);
+            this.txtMaCV.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(25, 112);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(112, 19);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Tên công viêc: ";
+            // 
+            // txtTenCV
+            // 
+            this.txtTenCV.Location = new System.Drawing.Point(140, 112);
+            this.txtTenCV.Name = "txtTenCV";
+            this.txtTenCV.Size = new System.Drawing.Size(227, 22);
+            this.txtTenCV.TabIndex = 4;
+            // 
+            // txtHeSoLuong
+            // 
+            this.txtHeSoLuong.Location = new System.Drawing.Point(550, 93);
+            this.txtHeSoLuong.Name = "txtHeSoLuong";
+            this.txtHeSoLuong.Size = new System.Drawing.Size(227, 22);
+            this.txtHeSoLuong.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(435, 93);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(97, 19);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Hệ số lương:";
+            // 
+            // dgridCongViec
+            // 
+            this.dgridCongViec.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgridCongViec.Location = new System.Drawing.Point(29, 155);
+            this.dgridCongViec.Name = "dgridCongViec";
+            this.dgridCongViec.RowHeadersWidth = 51;
+            this.dgridCongViec.RowTemplate.Height = 24;
+            this.dgridCongViec.Size = new System.Drawing.Size(748, 214);
+            this.dgridCongViec.TabIndex = 7;
+            this.dgridCongViec.Click += new System.EventHandler(this.dgridCongViec_Click);
+            // 
+            // btnThem
+            // 
+            this.btnThem.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnThem.Location = new System.Drawing.Point(29, 391);
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(90, 36);
+            this.btnThem.TabIndex = 8;
+            this.btnThem.Text = "Thêm";
+            this.btnThem.UseVisualStyleBackColor = true;
+            this.btnThem.Click += new System.EventHandler(this.btnThem_Click);
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnXoa.Location = new System.Drawing.Point(162, 391);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(90, 36);
+            this.btnXoa.TabIndex = 9;
+            this.btnXoa.Text = "Xóa";
+            this.btnXoa.UseVisualStyleBackColor = true;
+            this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
+            // 
+            // btnSua
+            // 
+            this.btnSua.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSua.Location = new System.Drawing.Point(287, 391);
+            this.btnSua.Name = "btnSua";
+            this.btnSua.Size = new System.Drawing.Size(90, 36);
+            this.btnSua.TabIndex = 10;
+            this.btnSua.Text = "Sửa";
+            this.btnSua.UseVisualStyleBackColor = true;
+            this.btnSua.Click += new System.EventHandler(this.btnSua_Click);
+            // 
+            // btnLuu
+            // 
+            this.btnLuu.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLuu.Location = new System.Drawing.Point(416, 391);
+            this.btnLuu.Name = "btnLuu";
+            this.btnLuu.Size = new System.Drawing.Size(90, 36);
+            this.btnLuu.TabIndex = 11;
+            this.btnLuu.Text = "Lưu";
+            this.btnLuu.UseVisualStyleBackColor = true;
+            this.btnLuu.Click += new System.EventHandler(this.btnLuu_Click);
+            // 
+            // btnBoqua
+            // 
+            this.btnBoqua.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBoqua.Location = new System.Drawing.Point(550, 391);
+            this.btnBoqua.Name = "btnBoqua";
+            this.btnBoqua.Size = new System.Drawing.Size(90, 36);
+            this.btnBoqua.TabIndex = 12;
+            this.btnBoqua.Text = "Bỏ qua";
+            this.btnBoqua.UseVisualStyleBackColor = true;
+            this.btnBoqua.Click += new System.EventHandler(this.btnBoqua_Click);
+            // 
+            // btnDong
+            // 
+            this.btnDong.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDong.Location = new System.Drawing.Point(687, 391);
+            this.btnDong.Name = "btnDong";
+            this.btnDong.Size = new System.Drawing.Size(90, 36);
+            this.btnDong.TabIndex = 13;
+            this.btnDong.Text = "Đóng";
+            this.btnDong.UseVisualStyleBackColor = true;
+            this.btnDong.Click += new System.EventHandler(this.btnDong_Click);
+            // 
+            // Congviec
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnDong);
+            this.Controls.Add(this.btnBoqua);
+            this.Controls.Add(this.btnLuu);
+            this.Controls.Add(this.btnSua);
+            this.Controls.Add(this.btnXoa);
+            this.Controls.Add(this.btnThem);
+            this.Controls.Add(this.dgridCongViec);
+            this.Controls.Add(this.txtHeSoLuong);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtTenCV);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtMaCV);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Congviec";
+            this.Text = "Congviec";
+            this.Load += new System.EventHandler(this.Congviec_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgridCongViec)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtMaCV;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtTenCV;
+        private System.Windows.Forms.TextBox txtHeSoLuong;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridView dgridCongViec;
+        private System.Windows.Forms.Button btnThem;
+        private System.Windows.Forms.Button btnXoa;
+        private System.Windows.Forms.Button btnSua;
+        private System.Windows.Forms.Button btnLuu;
+        private System.Windows.Forms.Button btnBoqua;
+        private System.Windows.Forms.Button btnDong;
+    }
+}
---
+++ TTCN/Forms/Congviec.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TTCN.Forms
+{
+    public partial class Congviec : Form
+    {
+        public Congviec()
+        {
+            InitializeComponent();
+        }
+
+        private void Congviec_Load(object sender, EventArgs e)
+        {
+            txtMaCV.Enabled = false;
+            btnLuu.Enabled = false;
+            btnBoqua.Enabled = false;
+            load_datagrid();
+        }
+        DataTable tblcv;
+        private void load_datagrid()
+        {
+            string sql;
+            sql = "select MaCV, TenCV, HeSoLuong from tblcongviec";
+            tblcv = Class.Function.Getdatatotable(sql);
+            dgridCongViec.DataSource = tblcv;
+            dgridCongViec.Columns[0].HeaderText = "Mã công việc";
+            dgridCongViec.Columns[1].HeaderText = "Tên công việc";
+            dgridCongViec.Columns[2].HeaderText = "Hệ số lương";
+            dgridCongViec.AllowUserToAddRows = false;
+            dgridCongViec.EditMode = DataGridViewEditMode.EditProgrammatically;
+        }
+
+        private void dgridCongViec_Click(object sender, EventArgs e)
+        {
+            if (btnThem.Enabled == false)
+            {
+                MessageBox.Show("Ban dang o che do them moi", "Thong bao", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if (tblcv.Rows.Count == 0)
+            {
+                MessageBox.Show("Khong co du lieu", "Thong bao", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            txtMaCV.Text = dgridCongViec.CurrentRow.Cells["MaCV"].Value.ToString();
+            txtTenCV.Text = dgridCongViec.CurrentRow.Cells["TenCV"].Value.ToString();
+            txtHeSoLuong.Text = dgridCongViec.CurrentRow.Cells["HeSoLuong"].Value.ToString();
+            btnSua.Enabled = true;
+            btnXoa.Enabled = true;
+            btnBoqua.Enabled = true;
+        }
+
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+
+            btnSua.Enabled = false;
+            btnXoa.Enabled = false;
+            btnBoqua.Enabled = true;
+            btnLuu.Enabled = true;
+            btnThem.Enabled = false;
+            resetvalue();
+            txtMaCV.Enabled = true;
+            txtMaCV.Focus();
+        }
+        private void resetvalue()
+        {
+            txtMaCV.Text = "";
+            txtTenCV.Text = "";
+            txtHeSoLuong.Text = "";
+        }
+
+        private void btnLuu_Click(object sender, EventArgs e)
+        {
+            string sql;
+            if (txtMaCV.Text == "")
+            {
+                MessageBox.Show("Bạn phải nhập mã chất liệu", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtMaCV.Focus();
+                return;
+            }
+            if (txtTenCV.Text.Trim().Length == 0)
+            {
+                MessageBox.Show("Bạn phải nhập tên chất liệu", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtTenCV.Focus();
+                return;
+            }
+            if (txtHeSoLuong.Text.Trim().Length == 0)
+            {
+                MessageBox.Show("Bạn phải nhập hệ số lương", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtHeSoLuong.Focus();
+                return;
+            }
+            sql = "SELECT MaCV FROM tblcongviec WHERE MaCV = N'" + txtMaCV.Text.Trim() + "'";
+            if (Class.Function.checkkey(sql))
+            {
+                MessageBox.Show("Mã chất liệu này đã có, bạn phải nhập mã khác", "Thôngbáo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtMaCV.Focus();
+                txtMaCV.Text = "";
+                return;
+            }
+            sql = "INSERT INTO tblcongviec(MaCV,TenCV, HeSoLuong) VALUES(N'" + txtMaCV.Text + "',N'" + txtTenCV.Text + "',N'" + txtHeSoLuong.Text + "')";
+            Class.Function.runsql(sql);
+            load_datagrid();
+            resetvalue();
+            btnXoa.Enabled = true;
+            btnThem.Enabled = true;
+            btnSua.Enabled = true;
+            btnBoqua.Enabled = false;
+            btnLuu.Enabled = false;
+            txtMaCV.Enabled = false;
+        }
+
+        private void btnSua_Click(object sender, EventArgs e)
+        {
+            string sql;
+            if (tblcv.Rows.Count == 0)
+            {
+                MessageBox.Show("Không còn dữ liệu!", "Thông báo",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information);
+                return;
+            }
+            if (txtMaCV.Text == "")
+            {
+                MessageBox.Show("Bạn chưa chọn bản ghi nào", "Thông báo",
+                MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+            if (txtTenCV.Text.Trim().Length == 0)
+            {
+                MessageBox.Show("Bạn phải nhập tên chất liệu", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtTenCV.Focus();
+                return;
+            }
+            sql = "UPDATE tblcongviec SET TenCV=N'" + txtTenCV.Text.ToString() + "' WHERE MaCV=N'" + txtMaCV.Text + "'";
+            Class.Function.runsql(sql);
+            load_datagrid();
+            resetvalue();
+            btnBoqua.Enabled = false;
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            string sql;
+            if (tblcv.Rows.Count == 0)
+            {
+                MessageBox.Show("Không còn dữ liệu!", "Thông báo",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information);
+                return;
+            }
+            if (txtMaCV.Text == "")
+            {
+                MessageBox.Show("Bạn chưa chọn bản ghi nào", "Thông báo",
+                MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+            if (MessageBox.Show("Bạn có muốn xóa không?", "Thông báo",
+            MessageBoxButtons.OKCancel, MessageBoxIcon.Question) == DialogResult.OK)
+            {
+                sql = "DELETE tblcongviec WHERE MaCV=N'" + txtMaCV.Text + "'";
+                Class.Function.RunSqlDel(sql);
+                load_datagrid();
+                resetvalue();
+            }
+        }
+
+        private void btnBoqua_Click(object sender, EventArgs e)
+        {
+            resetvalue();
+            btnBoqua.Enabled = false;
+            btnThem.Enabled = true;
+            btnXoa.Enabled = true;
+            btnSua.Enabled = true;
+            btnLuu.Enabled = false;
+            txtMaCV.Enabled = false;
+        }
+
+        private void btnDong_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
